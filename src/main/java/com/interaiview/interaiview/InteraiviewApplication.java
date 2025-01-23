1ff9449d500965f3a7ef7e312fb2df69ad1de170
KOMINPYO <gnjgnjgnj1@gmail.com>
2025-01-23 17:56:34 +0900

    feat: commitgo

---
+++ src/main/java/com/interaiview/interaiview/InteraiviewApplication.java
@@ -0,0 +1,13 @@
+package com.interaiview.interaiview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteraiviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteraiviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interaiview/interaiview/InteraiviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interaiview.interaiview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteraiviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
