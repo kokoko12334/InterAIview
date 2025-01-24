0be4d9a5e0ae84bb15054374118329788c48e59c
KOMINPYO <gnjgnjgnj1@gmail.com>
2025-01-25 00:49:20 +0900

    feat: 질문들

---
+++ src/main/java/com/interaiview/interaiview/dto/QuestionDTO.java
@@ -0,0 +1,12 @@
+package com.interaiview.interaiview.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class QuestionDTO {
+    private List<String> questions;
+}
