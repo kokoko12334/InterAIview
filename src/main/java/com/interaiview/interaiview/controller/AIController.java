0ad7ffb9aab2927cbd919392832838ef4a40f204
KOMINPYO <gnjgnjgnj1@gmail.com>
2025-01-25 18:09:17 +0900

    feat: AIController제작

---
+++ src/main/java/com/interaiview/interaiview/controller/AIController.java
@@ -0,0 +1,40 @@
+package com.interaiview.interaiview.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.interaiview.interaiview.dto.QaPairsDTO;
+import com.interaiview.interaiview.service.AIService;
+import com.interaiview.interaiview.util.FileValidator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/ai")
+public class AIController {
+
+    private final AIService aiService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<?> generateQuestion(@RequestParam("file123") MultipartFile file) throws IOException {
+        ResponseEntity<String> validationResponse = FileValidator.validateFile(file);
+
+        if (validationResponse != null) {
+            return validationResponse;
+        }
+
+        String fileType = FileValidator.getFileType(file);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(aiService.generateQuestion(file, fileType));
+    }
+
+    @PostMapping("/result")
+    public ResponseEntity<?> generateInterviewResult(@RequestBody QaPairsDTO qas) throws JsonProcessingException {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(aiService.generateInterviewResult(qas));
+    }
+}
