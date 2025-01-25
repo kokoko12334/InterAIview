3381a50bd65531e8435319cde5b6356ef06c0bdc
KOMINPYO <gnjgnjgnj1@gmail.com>
2025-01-25 18:11:16 +0900

    feat: 데이터전송

---
+++ src/main/java/com/interaiview/interaiview/dto/QaPairDTO.java
@@ -0,0 +1,12 @@
+package com.interaiview.interaiview.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class QaPairDTO {
+    private String question;
+    private String answer;
+    private String feedback;
+}
