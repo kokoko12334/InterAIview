95b1ddf4d7f51def39fdeb03080fec66dcd1c35a
KOMINPYO <gnjgnjgnj1@gmail.com>
2025-01-28 21:37:05 +0900

    -

--- src/main/java/com/interaiview/interaiview/InteraiviewApplication.java
+++ src/main/java/com/interaiview/interaiview/InteraiviewApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InteraiviewApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(InteraiviewApplication.class, args);
 	}
