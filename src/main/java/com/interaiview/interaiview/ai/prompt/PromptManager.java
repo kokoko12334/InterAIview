30d42f9cc659b816d876cfed14d8fe3df1cc6419
KOMINPYO <gnjgnjgnj1@gmail.com>
2025-02-08 22:58:58 +0900

    feat: 질문개수 변경

--- src/main/java/com/interaiview/interaiview/ai/prompt/PromptManager.java
+++ src/main/java/com/interaiview/interaiview/ai/prompt/PromptManager.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum PromptManager {
-    GENERATE_QUESTIONS("You are an interviewer. Based on the resume details, generate 3 interview questions and include the intent of each question in the \"tip\" section with a single sentence. with the answer sections left blank and feedback sections left blank in the format(Do not wrap the json codes in JSON markers): { \"qas\": [{\"question\":, \"tip\":, \"answer\":\"\", \"feedback\":\"\"},...]}"),
+    GENERATE_QUESTIONS("You are an interviewer. Based on the resume details, generate 10 interview questions and include the intent of each question in the \"tip\" section with a single sentence. with the answer sections left blank and feedback sections left blank in the format(Do not wrap the json codes in JSON markers): { \"qas\": [{\"question\":, \"tip\":, \"answer\":\"\", \"feedback\":\"\"},...]}"),
     GENERATE_RESULT("You are the world's best interview advisor. Based on the given question and the answer, provide feedback for each question and answer. If the answer is empty, mark the feedback as \"empty\" And add an overall feedback at the end.(Keep the original questions, answers as they are) The output format(Do not wrap the json codes in JSON markers) is { \"qas\": [{\"question\":, \"tip\":, \"answer\":\"\", \"feedback\":\"\"},...], \"result\": \"\"}");
 
     private final String prompt;
